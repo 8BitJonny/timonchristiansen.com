87db585e6e3697fc0171710712b74f96785fb47c
8BitJonny <timon99@gmx.de>
2020-05-05 00:43:39 +0200

    :tada: landing page and setup

---
+++ components/HomeScreen.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home-screen">
+    <div class="greeting">
+      <h1>This is my<br>digital playground</h1>
+      <Button text="Stay tuned" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "~/components/ui/button.vue";
+export default {
+  name: 'HomeScreenVue',
+  components: { Button }
+}
+</script>
+
+<style scoped>
+.home-screen {
+  background: #2A2626;
+  @apply w-screen h-screen flex justify-center items-center;
+}
+
+.greeting {
+  @apply text-center;
+}
+
+.greeting > h1 {
+  @apply mb-10;
+}
+</style>
---
+++ components/ui/button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="standard-button">{{ text }}</button>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Button',
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.standard-button {
+  color: #2A2626;
+  font-size: 1.2rem;
+  background-color: white;
+  padding: 0.8rem 1.6rem;
+  border-radius: 1.5rem;
+  @apply outline-none select-none;
+}
+</style>
